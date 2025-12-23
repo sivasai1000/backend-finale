98f14f94ac7bb5b4d135ca8d84ccf54e0c2c0d57
Mallipudi Devi Siva Sai <sivasaimallipudi9h@gmail.com>
2025-12-23 19:42:13 +0530

    checked

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,8 +13,12 @@ const protect = async (req, res, next) => {
 
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            
+
+
             const [rows] = await pool.query('SELECT * FROM Users WHERE id = ?', [decoded.user.id]);
+            if (rows.length === 0) {
+                return res.status(401).json({ message: 'User not found' });
+            }
             req.user = rows[0];
 
             next();
